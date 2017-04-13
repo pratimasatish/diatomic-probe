6f070fdf90dc4fe685599b5d04eef4b4d1c4671d
Pratima Satish <pratimasatish@berkeley.edu>
2017-04-12 17:17:33 -0700

    adding dye to CdS surface

--- make_config.py
+++ make_config.py
@@ -20,10 +20,14 @@ by0 = bottom[0,2]
 bz0 = bottom[0,3]
 sx0 = np.min(liq_hex[:,1])
 sy0 = np.min(liq_hex[:,2])
-sz0 = np.min(liq_hex[:,3])
+sz0 = np.min(dye[:,3])
+dx0 = np.min(dye[:,1])
+dy0 = np.min(dye[:,2])
+dz0 = np.min(dye[:,3])
 centred_data = np.zeros( (len(data), 3) )
 centred_bottom = np.zeros( (len(bottom), 3) )
 centred_hex = np.zeros( (len(liq_hex), 3) )
+centred_dye = np.zeros( (len(dye), 3) )
 nx = args.nx
 nz = args.nz
 n_molecules = nx * nz
@@ -39,6 +43,11 @@ n_hexmolecules = n_hexatoms / n_hex
 n_hexbonds = n_hex - 1
 n_hexangles = n_hex - 2
 n_hexdihedrals = n_hex - 3
+n_dye = len(dye)
+n_dyebonds = n_dye + 2
+n_dyeangles = n_dye + 6
+n_dyedihedrals = 9
+n_dyeimpropers = 5
 xlo =  -5.0
 xhi =  80.0
 ylo = -124.0
@@ -49,7 +58,7 @@ zhi =  80.0
 # print n_hexdihedrals * n_hexmolecules
 # print n_bottommolecules * n_dihedrals
 
-# rotate = np.array([ [np.cos(0.5 * np.pi), -np.sin(0.5 * np.pi), 0], [np.sin(0.5 * np.pi), np.cos(0.5 * np.pi), 0], [0, 0, 1.0] ])
+rotate = np.array([ [np.cos(0.5 * np.pi), 0, -np.sin(0.5 * np.pi)], [0, 1, 0], [np.sin(0.5 * np.pi), 0,  np.cos(0.5 * np.pi)] ])
 
 # centre the original backbone
 for i in range(len(data)):
@@ -69,15 +78,38 @@ for i in range(len(liq_hex)):
     centred_hex[i,1] = liq_hex[i,2] + 20.0
     centred_hex[i,2] = liq_hex[i,3]
 
-# for i in range(len(centred_data)):
-#     centred_data[i, :] = rotate.dot(centred_data[i, :])
+# centre the hex chain
+for i in range(len(dye)):
+    centred_dye[i,0] = dye[i,1] - dx0 + 4.12 * 10
+    centred_dye[i,1] = dye[i,2] - dy0
+    centred_dye[i,2] = dye[i,3] - dz0 + 6.75 * 6
+
+# rotate the dye molecule to be upright
+# for i in range(len(centred_dye)):
+#     centred_dye[i, :] = rotate.dot(centred_dye[i, :])
 
 # print sy0, min(centred_hex[:,1])
 
 if args.fmt == "xyz":
-    print "{}".format(n_molecules * n_chain + n_molecules * 16 + 2 * n_hexatoms + n_bottom)
+    print "{}".format( (n_molecules - 5) * n_chain + n_molecules * 16 + 2 * n_hexatoms + n_bottom + n_dye)
     print "Comment Line"
     nmol = 0
+
+    # dye on surface
+    for i in range(n_dye):
+        if (i == 6):
+            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( centred_dye[i,0], centred_dye[i,1], centred_dye[1,2] )
+        elif (i == 7): 
+            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( centred_dye[i,0], centred_dye[i,1], centred_dye[1,2] )
+        elif (i == 8):
+            print "5	{:4.5f}	{:4.5f}	{:4.5f}".format( centred_dye[i,0], centred_dye[i,1], centred_dye[1,2] )
+        elif (i == 12 or i == 13 or i ==9 or i == 10):
+            print "6	{:4.5f}	{:4.5f}	{:4.5f}".format( centred_dye[i,0], centred_dye[i,1], centred_dye[1,2] )
+        elif (i > 8 and i < 22):
+            print "7	{:4.5f}	{:4.5f}	{:4.5f}".format( centred_dye[i,0], centred_dye[i,1], centred_dye[1,2] )
+        else:
+            print "2	{:4.5f}	{:4.5f}	{:4.5f}".format( centred_dye[i,0], centred_dye[i,1], centred_dye[1,2] )
+
     # atomic coordinates
     for i in range(nx):
         for j in range(nz):
@@ -85,7 +117,11 @@ if args.fmt == "xyz":
             # shift molecule
             coords[:, 0] = coords[:, 0] + 4.12 * i
             coords[:, 2] = coords[:, 2] + 6.75 * j
-        
+
+            # skip dye position
+            if (i >= 10 and i <=14 and j == 6):
+                continue
+       
             for k in range(n_chain):
                 if (k == n_chain-1):
                     print "1       {:4.5f} {:4.5f} {:4.5f}".format( coords[k,0], coords[k,1], coords[k,2] )
@@ -100,31 +136,31 @@ if args.fmt == "xyz":
             coords = centred_data[0, :] - np.array([0.0, 1.56, 0.0])
             coords[0] = coords[0] + 4.12 * i
             coords[2] = coords[2] + 6.75 * j
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1], coords[2] )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1], coords[2] + 2.5299 )
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 1.18977, coords[2] + 3.375 )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 1.18977, coords[2] - 3.375 + 2.5299 )
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 3.5693, coords[2] )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 3.5693, coords[2] + 2.5299 )
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 3.5693 - 1.18977, coords[2] + 3.375 )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 3.5693 - 1.18977, coords[2] - 3.375 + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1], coords[2] )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1], coords[2] + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 1.18977, coords[2] + 3.375 )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 1.18977, coords[2] - 3.375 + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 3.5693, coords[2] )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 3.5693, coords[2] + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 3.5693 - 1.18977, coords[2] + 3.375 )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 3.5693 - 1.18977, coords[2] - 3.375 + 2.5299 )
 
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848, coords[2] )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848, coords[2] + 2.5299 )
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 1.18977, coords[2] + 3.375 )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 1.18977, coords[2] - 3.375 + 2.5299 )
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 3.5693, coords[2] )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 3.5693, coords[2] + 2.5299 )
-            print "3	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848 - 3.5693 - 1.18977, coords[2] + 3.375 )
-            print "4	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848 - 3.5693 - 1.18977, coords[2] - 3.375 + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848, coords[2] )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848, coords[2] + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 1.18977, coords[2] + 3.375 )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 1.18977, coords[2] - 3.375 + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 3.5693, coords[2] )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0] - 2.06073, coords[1] - 7.13848 - 3.5693, coords[2] + 2.5299 )
+            print "9	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848 - 3.5693 - 1.18977, coords[2] + 3.375 )
+            print "8	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[0], coords[1] - 7.13848 - 3.5693 - 1.18977, coords[2] - 3.375 + 2.5299 )
 
     # hex molecules here
     coords = centred_hex + np.array([0.0, 0.0, 0.0])
     for i in range(n_hexatoms):    
         if ((i%6 == 0) or ((i+1)%6 == 0)):
-            print "5	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
+            print "10	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
         else:
-            print "6	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
+            print "11	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
 
     # bottom oleic acid ligands here
     coords = centred_bottom + np.array([0.0, 0.0, 0.0])
@@ -138,22 +174,24 @@ if args.fmt == "xyz":
     coords = centred_hex + np.array([0.0, -55.5 - 80, 0.0])
     for i in range(n_hexatoms):    
         if ((i%6 == 0) or ((i+1)%6 == 0)):
-            print "5	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
+            print "10	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
         else:
-            print "6	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
+            print "11	{:4.5f}	{:4.5f}	{:4.5f}".format( coords[i,0], coords[i,1], coords[i,2] )
 
 if args.fmt == "lmp":
     # beginning of LAMMPS input file
     print "LAMMPS coords for {} oleic acid-like molecules\n".format( n_molecules )
-    print "{}	atoms".format( n_molecules * n_chain + n_molecules * 16 + 2 * n_hexatoms + n_bottom )
-    print "{}	bonds".format( n_molecules * n_bonds + n_molecules + 2 * n_hexmolecules * n_hexbonds + n_bottommolecules * n_bonds )
-    print "{}	angles".format( n_molecules * n_angles + 2 * n_hexmolecules * n_hexangles + n_bottommolecules * n_angles )
-    print "{}	dihedrals".format( n_molecules * n_dihedrals + 2 * n_hexmolecules * n_hexdihedrals + n_bottommolecules * n_dihedrals )
+    print "{}	atoms".format( (n_molecules - 5) * n_chain + n_molecules * 16 + 2 * n_hexatoms + n_bottom + n_dye )
+    print "{}	bonds".format( n_molecules * n_bonds + n_molecules + 2 * n_hexmolecules * n_hexbonds + n_bottommolecules * n_bonds+ n_dyebonds )
+    print "{}	angles".format( n_molecules * n_angles + 2 * n_hexmolecules * n_hexangles + n_bottommolecules * n_angles + n_dyeangles )
+    print "{}	dihedrals".format( n_molecules * n_dihedrals + 2 * n_hexmolecules * n_hexdihedrals + n_bottommolecules * n_dihedrals + n_dyedihedrals )
+    print "{} impropers".format( n_dyeimpropers )
     
-    print "6	atom types"
-    print "1	bond types"
-    print "1	angle types"
-    print "1	dihedral types"
+    print "11	atom types"
+    print "3	bond types"
+    print "3	angle types"
+    print "3	dihedral types"
+    print "1	improper types"
     
     print "{} {}	xlo xhi".format(xlo, xhi)
     print "{} {}	ylo yhi".format(ylo, yhi)
